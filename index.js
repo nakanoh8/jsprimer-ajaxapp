7acd18b55b70ff03c5f7409d042bc3e092165856
nakanoh <murry.xxx.7@gmail.com>
2020-05-28 01:08:48 +0900

    ~HTTP

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// import { response } from "express";
+
 console.log("index.js: loaded");
 
 const heading = document.querySelector("h2");
@@ -7,3 +9,12 @@ console.log(headingText);
 const button = document.createElement("button");
 button.textContent = "Push Me";
 document.body.appendChild(button);
+
+const userId = "nananakano000";
+fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+    .then(response => {
+        console.log(response.status);
+        return response.json().then(userInfo => {
+            console.log(userInfo);
+        });
+    });
