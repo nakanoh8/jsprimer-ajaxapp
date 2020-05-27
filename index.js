d1ccee8800199bd6a1c59d63c39e3ddf12474443
nakanoh <murry.xxx.7@gmail.com>
2020-05-28 00:54:32 +0900

    ~エントリーポイント

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("index.js: loaded");
+
+const heading = document.querySelector("h2");
+const headingText = heading.textContent;
+console.log(headingText);
+
+const button = document.createElement("button");
+button.textContent = "Push Me";
+document.body.appendChild(button);
