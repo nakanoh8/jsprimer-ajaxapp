b5c9cb4b6a2283d3818d7b8cc6a74d3996ce7422
nakanoh <murry.xxx.7@gmail.com>
2020-05-28 10:31:40 +0900

    ~HTTP通信：fetchUserInfo()を作成

--- index.js
+++ index.js
@@ -10,11 +10,36 @@ const button = document.createElement("button");
 button.textContent = "Push Me";
 document.body.appendChild(button);
 
-const userId = "nananakano000";
-fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+function fetchUserInfo(userId){
+    fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
     .then(response => {
         console.log(response.status);
-        return response.json().then(userInfo => {
-            console.log(userInfo);
-        });
+        if(!response.ok){
+            console.error("エラーレスポンス", response);
+        }else{
+            return response.json().then(userInfo => {
+                console.log(userInfo);
+            });
+        }
+    }).catch(error => {
+        console.error(error);
     });
+}
+
+function fetchUserInfoByXHR(userId){
+    const req = new XMLHttpRequest();
+    req.open("GET", `https://api.github.com/users/${encodeURIComponent(userId)}`);
+    req.addEventListener("load", () => {
+        if(req.status >= 200 && req.status < 300){
+            const userInfo = JSON.parse(req.responseText);
+            console.log(userInfo);
+        }else{
+            console.error("エラーレスポンス", req.statusText);
+        }
+    });
+    req.addEventListener("error", () => {
+        console.error("ネットワークエラー");
+    });
+
+    req.send();
+}
